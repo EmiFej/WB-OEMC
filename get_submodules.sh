4e6fc9363f1d1fd243ca59df6c61b55fdf59d6f2
eliasinul <elias_islam@sfu.ca>
2025-04-27 09:19:56 -0700

    file cleanups

--- get_submodules.sh
+++ get_submodules.sh
@@ -2,16 +2,13 @@
 echo "Initializing and updating submodules..."
 git submodule update --init --recursive
 
-# Navigate to the Linking_tool submodule and install it
+# Navigate to the submodule and install local packages
 if [ -f models/Linking_tool/setup.py ]; then
-    echo "Installing packages from setup.py..."
+    echo "Installing packages from submodules..."
     pip install -e models/RESource
-    pip install -e models/BC_Nexus
-    pip install -e models/PyPSA_BC
+    pip install -e models/osemosys_global
+    pip install -e models/pypsa_eur
 
-else
-    echo "setup.py not found in Linking_tool. Skipping installation."
-fi
 
 echo "Submodules initialized and packages installed successfully."
 git submodule status
